e86d78dc86d90ac225f8eac9192353b8a980c117
Donald Iloekwe <donytex4u@gmail.com>
2023-06-12 20:40:20 +0100

    fix: change build output directory + build command

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: '.next',
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'dist',
 };
 
 module.exports = nextConfig;
