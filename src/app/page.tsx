626265ec91afeb3257c1977ceda77a6337457738
Donald Iloekwe <donytex4u@gmail.com>
2023-06-24 15:08:41 +0100

    setup zustand store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import React from 'react';
+import useMovieStore from '../store/store';
 
 const page = () => {
+  const movies = useMovieStore((state) => state.movies);
+  const setError = useMovieStore((state) => state.setError);
   return <div>page</div>;
 };
 
