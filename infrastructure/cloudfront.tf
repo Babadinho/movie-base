e24eb96c5f437dfdd3673598620c572ac5cf2899
Donald Iloekwe <donytex4u@gmail.com>
2023-06-24 11:47:19 +0100

    fix IllegalOriginAccessConfiguration error on terraform apply

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -4,12 +4,6 @@
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
     domain_name              = aws_s3_bucket.movie_base_s3_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
     origin_id                = aws_s3_bucket.movie_base_s3_bucket.id
